0bea62d4e8f4b0cf0475567a12062d0af752a5c6
haroxdev <harox@haroxsec.net>
2024-03-11 11:26:15 +0100

    Validate name

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function VsCode() {
   return (
     <iframe
-      src="https://github1s.com/haroxdev/harox_portfolio/blob/main/components/apps/harox.js"
+      src="https://github1s.com/haroxdev/harox-portfolio/blob/main/components/apps/harox.js"
       frameBorder="0"
       title="VsCode"
       className="h-full w-full bg-ub-cool-grey"
