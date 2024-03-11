a27da67d3d53cf8197851f34552f66a60789147a
haroxdev <harox@haroxsec.net>
2024-03-11 11:52:46 +0100

    Vercel Deploy

--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -4,7 +4,6 @@ export default function VsCode() {
   return (
     <iframe
       src="https://github1s.com/haroxdev/harox-portfolio/blob/complete_beta/components/apps/harox.js"
-      frameBorder="0"
       title="VsCode"
       className="h-full w-full bg-ub-cool-grey"
     ></iframe>
